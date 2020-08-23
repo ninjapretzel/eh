1e91c0df2a1d750d47dd54ee4f331079a1e978b7
ninjapretzel <ninjapretzel@yahoo.com>
2020-08-22 23:47:44 -0600

    Adds some jsdoc

--- duckpunches/core/string.js
+++ duckpunches/core/string.js
@@ -1,8 +1,15 @@
 
-
-String.prototype.replaceAll = function(pattern, replace) {
-	const actualMatch = new RegExp(pattern, "g");
-	return this.replace(actualMatch, replace);
+// Polyfills are duckpunches.
+if (!String.prototype.replaceAll) {
+/** Partial polyfill for ES262 replaceAll.
+See: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replaceAll 
+@param {string} pattern - Pattern to match (must be string) 
+@param {string} replace - String to replace matches with 
+@returns {string} replaced string value*/
+	String.prototype.replaceAll = function(pattern, replace) {
+		const actualMatch = new RegExp(pattern, "g");
+		return this.replace(actualMatch, replace);
+	}
 }
 
 module.exports = "STRING"
