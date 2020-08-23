7158a4f6d46c8b35a21eb0d662c81ae1460277d4
ninjapretzel <ninjapretzel@yahoo.com>
2020-08-22 21:05:47 -0600

    Stand up server

---
+++ app.js
@@ -0,0 +1,50 @@
+// Static setup + Builtin dependencies
+const fs_ = require("fs");
+const fs = require("fs-promise-native");
+const IS_DEV = fs_.existsSync("./.dev");
+
+
+// Package dependencies
+const Koa = require('koa');
+const views = require("koa-views");
+const bodyParser = require("koa-bodyparser");
+const static = require("koa-static");
+// @publish: pseudo-package. Maybe publish eventually?
+const layout = require("./middleware/koa-layout"); 
+
+// Own code dependencies
+// const db = require("./db");
+// @TODO: Make these a part of config.
+const HOST = IS_DEV ? "localhost" : "eehhh.westus.cloudapp.azure.com"
+const PORT = IS_DEV ? 3000 : 80;
+const HTTPS = false;
+
+const app = new Koa();
+app.use(bodyParser());
+app.use(static("public"));
+app.use(layout("main"));
+
+app.use(views(`${__dirname}/views`,
+	{ extension: "handlebars" },
+	{ map: { handlebars: "handlebars" } }
+))
+
+async function main() {
+	
+	// await db.sequelize.sync({ force: true });
+	
+	app.on('error', err => { console.error("internal server error:", err); });
+
+	app.listen(PORT, function() {
+		let fullhost = `${HTTPS ? "https" : "http"}://${HOST}`;
+		if (PORT !== 80 && PORT !== 443) {
+			fullhost += `:${PORT}`;
+		}
+		
+		console.log(`App listening at ${fullhost}`);
+	});
+	
+}
+
+
+main();
